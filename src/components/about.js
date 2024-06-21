f62fe819eca684d604927aae5b48de555b6a48ef
vivianritah <143530424+vivianritah@users.noreply.github.com>
2024-06-21 15:28:05 +0300

    commit

--- src/components/about.js
+++ src/components/about.js
@@ -1,36 +1,35 @@
 // About.js
 import React from 'react';
-import './about.css'
-
+import './about.css';
 
 function About() {
   return (
     <div className="about-container">
-      <section className="motto-section">
-        <h2 className="motto-title">Our Motto</h2>
-        <p className="motto">Grow In Stature, Wisdom And Spirit</p>
-      </section>
-      <section className="info-section">
-        <div className="vision-mission">
-          <h3 className="info-title">Vision</h3>
-          <p className="info-text">A joyful and prosperous generation standing for the truth and integrity.</p>
+      <div className="content-wrapper">
+        <h1 className="page-title">About Us</h1>
+        <div className="section motto-section">
+          <h2 className="title">Our Motto</h2>
+          <p className="text">Grow In Stature, Wisdom And Spirit</p>
         </div>
-      
-        <div className="vision-mission">
-          <h3 className="info-title">Mission</h3>
-          <p className="info-text">To raise a God-fearing generation through a holistic quality education.</p>
+        <div className="section vision-section">
+          <h2 className="title">Vision</h2>
+          <p className="text">A joyful and prosperous generation standing for the truth and integrity.</p>
         </div>
-        <div className="values-section">
-          <h3 className="values-title">Core Values</h3>
+        <div className="section mission-section">
+          <h2 className="title">Mission</h2>
+          <p className="text">To raise a God-fearing generation through a holistic quality education.</p>
+        </div>
+        <div className="section values-section">
+          <h2 className="title">Core Values</h2>
           <ul className="values-list">
             <li>God-fearing</li>
             <li>Bible-Centred</li>
             <li>Integrity</li>
-            <li>Team work</li>
+            <li>Teamwork</li>
             <li>Accountability</li>
           </ul>
         </div>
-      </section>
+      </div>
     </div>
   );
 }
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,51 @@
-// Contact Component
+
+
+
+
+
+// Contact.js
+import React from 'react';
+import './contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faYoutube, faTiktok } from '@fortawesome/free-brands-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import logo from '../images/badge.png'; 
+
 function Contact() {
-    return (
-      <div className="contact">
-        <h1>Contact Us</h1>
+  return (
+    <div className="contact-container">
+      <header className="header">
+        <img src={logo} alt="Innerman logo" className="logo" />
+        <div className="contact-info">
+          <p>0414 342 684 | 0757 211 737 |</p>
+        </div>
+      </header>
+      <div className="content-wrapper">
+        <h1 className="page-title">Innerman Pre & Primary School</h1>
+        <div className="address-section">
+          <FontAwesomeIcon icon={faMapMarkerAlt} className="location-icon" />
+          <div className="address-details">
+            <p><strong>Innerman Pre & Primary School</strong></p>
+            <p> Kimera Road</p>
+            <p>Kasuubi, Kampala</p>
+            <p>P.O.Box 246, Kampala Uganda</p>
+            <p>+256(0)757 211737 | +256(0)757 211344</p>
+          </div>
+        </div>
+        <div className="social-media">
+          <a href="https://www.facebook.com/search/top?q=innerman%20pre%20and%20primary%20school" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="https://www.youtube.com/results?search_query=innerman+pre+and+primary+school" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <FontAwesomeIcon icon={faYoutube} />
+          </a>
+          <a href="https://www.tiktok.com/search?q=innerman%20pre%20and%20primary%20school%20kasubi&t=1718969647805" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <FontAwesomeIcon icon={faTiktok} />
+          </a>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Contact;
